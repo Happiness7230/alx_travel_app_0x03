b79d3afee744707640487c80652a3a9203b081a5
Your NameHappiness <you@example.comobochihappiness180@gmail.com>
2025-11-09 09:58:40 +0000

    Swagger UI documentation Added for deployment

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listings'
+    'rest_framework',     
+    'drf_yasg',             
+    'listings', 
 ]
 
 MIDDLEWARE = [
